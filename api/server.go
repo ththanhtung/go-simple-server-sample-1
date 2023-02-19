3de184e5a00c9591e5ec5432979c6b322b6ed169
Thanh Tung <nguyenththanhtung317@gmail.com>
2023-02-20 03:13:11 +0700

    finish code

--- api/server.go
+++ api/server.go
@@ -3,13 +3,14 @@ package api
 import (
 	"encoding/json"
 	"net/http"
+
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 )
 
-type Item struct{
-	ID uuid.UUID `json:"id"`
-	Name string `json:"name"`
+type Item struct {
+	ID   uuid.UUID `json:"id"`
+	Name string    `json:"name"`
 }
 
 type Server struct {
@@ -20,10 +21,17 @@ type Server struct {
 
 func NewServer() *Server {
 	s := &Server{
-		Router: mux.NewRouter(),
+		Router:        mux.NewRouter(),
 		shoppingItems: []Item{},
 	}
+	s.routes()
 	return s
+}
+
+func (s *Server) routes() {
+	s.HandleFunc("/shopping-item", s.createShoppingItem()).Methods("POST")
+	s.HandleFunc("/shopping-item", s.GetShoppingItems()).Methods("GET")
+	s.HandleFunc("/shopping-item/{id}", s.RemoveShoppingItem()).Methods("DELETE")
 }
 
 func (s *Server) createShoppingItem() http.HandlerFunc {
@@ -38,36 +46,35 @@ func (s *Server) createShoppingItem() http.HandlerFunc {
 		i.ID = uuid.New()
 		s.shoppingItems = append(s.shoppingItems, i)
 
-		w.Header().Set("content-type","application/json")
+		w.Header().Set("content-type", "application/json")
 
-		if err:= json.NewEncoder(w).Encode(i); err != nil {
+		if err := json.NewEncoder(w).Encode(i); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 	}
 }
 
-
-func (s *Server) GetShoppingItems() http.HandlerFunc{
+func (s *Server) GetShoppingItems() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("content-type","application/json")
-		if err := json.NewEncoder(w).Encode(s.shoppingItems); err != nil{
+		w.Header().Set("content-type", "application/json")
+		if err := json.NewEncoder(w).Encode(s.shoppingItems); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 	}
 }
 
-func (s *Server) RemoveShoppingItem() http.HandlerFunc{
+func (s *Server) RemoveShoppingItem() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		idStr, _ := mux.Vars(r)["id"]
 		id, err := uuid.Parse(idStr)
-		if (err != nil){
-			http.Error(w,err.Error(),http.StatusBadRequest)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
 		}
 
 		for i, item := range s.shoppingItems {
-			if (item.ID == id){
+			if item.ID == id {
 				s.shoppingItems = append(s.shoppingItems[:i], s.shoppingItems[i+1:]...)
 				break
 			}
--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"net/http"
+	"example.com/packages/api"
+)
+
 func main() {
-	
+	srv := api.NewServer()
+	http.ListenAndServe(":8080", srv)
 }
