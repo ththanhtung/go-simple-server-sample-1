5e929f83b1fadcdf93dca2898fce296922fabcb5
Thanh Tung <nguyenththanhtung317@gmail.com>
2023-02-20 02:38:16 +0700

    feat add server and createShoppingItem route

---
+++ api/server.go
@@ -0,0 +1,48 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+)
+
+type Item struct{
+	ID uuid.UUID `json:"id"`
+	Name string `json:"name"`
+}
+
+type Server struct {
+	*mux.Router
+
+	shoppingItems []Item
+}
+
+func NewServer() *Server {
+	s := &Server{
+		Router: mux.NewRouter(),
+		shoppingItems: []Item{},
+	}
+	return s
+}
+
+func (s *Server) createShoppingItem() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var i Item
+
+		if err := json.NewDecoder(r.Body).Decode(&i); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		i.ID = uuid.New()
+		s.shoppingItems = append(s.shoppingItems, i)
+
+		w.Header().Set("content-type","application/json")
+
+		if err:= json.NewEncoder(w).Encode(i); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
